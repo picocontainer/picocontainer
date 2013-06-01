7900c7a15e8497c28e0050c2d39edbe02f7f1cc3
Michael Rimov <mike.rimov@centercomp.com>
2013-06-01 02:11:00 -0700

    Update to new api

--- script/script-testmodel/src/java/org/picocontainer/script/testmodel/TestInjection.java
+++ script/script-testmodel/src/java/org/picocontainer/script/testmodel/TestInjection.java
@@ -1,13 +1,15 @@
 package org.picocontainer.script.testmodel;
 
+import java.util.Properties;
+
 import org.picocontainer.ComponentAdapter;
-import org.picocontainer.Parameter;
-import org.picocontainer.PicoCompositionException;
 import org.picocontainer.ComponentMonitor;
 import org.picocontainer.LifecycleStrategy;
+import org.picocontainer.PicoCompositionException;
 import org.picocontainer.injectors.AdaptingInjection;
-
-import java.util.Properties;
+import org.picocontainer.parameters.ConstructorParameters;
+import org.picocontainer.parameters.FieldParameters;
+import org.picocontainer.parameters.MethodParameters;
 
 /**
  * @author Paul Hammant
@@ -21,11 +23,11 @@ public final class TestInjection extends AdaptingInjection {
         this.sb = sb;
     }
 
-    @SuppressWarnings("unchecked")
-    public ComponentAdapter createComponentAdapter(ComponentMonitor monitor, LifecycleStrategy lifecycle,
-                    Properties componentProps, Object key, Class impl, Parameter... parameters) throws PicoCompositionException {
+    @Override
+    public  <T> ComponentAdapter<T>  createComponentAdapter(ComponentMonitor monitor, LifecycleStrategy lifecycle,
+                    Properties componentProps, Object key, Class<T> impl, ConstructorParameters constructorParams, FieldParameters[] fieldParams, MethodParameters[] methodParams) throws PicoCompositionException {
         sb.append("called");
         return super.createComponentAdapter(monitor, lifecycle,
-                                            componentProps, key, impl, parameters);
+                                            componentProps, key, impl, constructorParams, fieldParams, methodParams);
     }
 }
