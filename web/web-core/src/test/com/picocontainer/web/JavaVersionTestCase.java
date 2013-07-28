4211c272ad90ed11f49661f70cbb097c8461013a
Paul Hammant <paul@hammant.org>
2013-07-28 17:40:35 -0500

    improved

--- web/web-core/src/test/com/picocontainer/web/JavaVersionTestCase.java
+++ web/web-core/src/test/com/picocontainer/web/JavaVersionTestCase.java
@@ -2,7 +2,7 @@ package com.picocontainer.web;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.junit.Assert.assertThat;
 
 public class JavaVersionTestCase {
@@ -11,7 +11,7 @@ public class JavaVersionTestCase {
     public void javaVersionShouldBeJdkSevenOrAboveBecauseServletApiVersionRequiresThatDuringCompilation() {
 
         String ver = System.getProperty("java.runtime.version");
-        assertThat(ver, startsWith("1.7"));
+        assertThat(ver, greaterThanOrEqualTo("1.7"));
 
     }
 
